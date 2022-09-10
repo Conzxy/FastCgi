3d1b6ee155c0a23a94b9d2ba162499e7f1309613
Conzxy <1967933844@qq.com>
2022-09-10 22:48:52 +0800

    send once

--- example/echo_cgi.cc
+++ example/echo_cgi.cc
@@ -5,6 +5,9 @@
 
 using namespace fcgi;
 using namespace kanon;
+using namespace std;
+
+static string PATH = "/echo/";
 
 class EchoCgiServer : kanon::noncopyable {
  public:
@@ -16,13 +19,20 @@ class EchoCgiServer : kanon::noncopyable {
         auto codec = new FcgiCodec(conn);
         codec->SetRequestHandler(
             [codec](TcpConnectionPtr const &conn, FcgiRequest request) {
-              auto in = std::move(request.stdin_stream);
-              codec->SendStdout(conn, request.request_id, "Context-Type: text/plain\r\n\r\n");
-              codec->SendStdout(conn, request.request_id, in.GetReadBegin(),
-                                in.GetReadableSize());
-              in.AdvanceAll();
-              codec->EndStdout(conn, request.request_id);
-              codec->EndRequest(conn, request.request_id);
+              std::string buffer;
+              buffer.reserve(4096);
+              buffer.append("Context-Type: text/plain\r\n\r\n");
+              StringView uri(request.param_map["REQUEST_URI"]);
+              StringView message = uri.substr(PATH.size());
+              buffer.append(message.data(), message.size());
+              codec->SendStdout(conn, request, buffer.data(), buffer.size());
+              // codec->SendStdout(conn, request.request_id, "Context-Type: text/plain\r\n\r\n");
+              // codec->SendStdout(conn, request.request_id, message);
+              // codec->SendStdout(conn, request.request_id, in.GetReadBegin(),
+              //                   in.GetReadableSize());
+              // in.AdvanceAll();
+              codec->EndStdout(conn, request);
+              codec->EndRequest(conn, request);
             });
         conn->SetContext(codec);
       } else {
@@ -32,6 +42,7 @@ class EchoCgiServer : kanon::noncopyable {
   }
   
   void Listen() { server_.StartRun(); }
+  void SetLoopNum(int num) { server_.SetLoopNum(num); }
  private:
   TcpServer server_;
 };
@@ -40,13 +51,40 @@ int main(int argc, char *argv[])
 {
   // kanon::SetKanonLog(false);
   uint16_t port = 9999;
-  if (argc > 1) {
-    port = ::atoi(argv[1]);
+  int thread_num = 0;  
+  std::vector<char const *> args;
+  while (argc > 1) {
+    if (strcmp(argv[argc-1], "-p") == 0) {
+      if (args.size() != 1) {
+        fprintf(stderr, "No port number");
+        exit(0);
+      }
+      port = atoi(args[0]);
+      args.clear();
+    } else if (strcmp(argv[argc-1], "-s") == 0) {
+      if (args.size() != 0) {
+        fprintf(stderr, "silent mode no arguments");
+        exit(0);
+      }
+      LOG_INFO << "Running in silent mode";
+      kanon::EnableAllLog(false);
+    } else if (strcmp(argv[argc-1], "-t") == 0) {
+      if (args.size() != 1) {
+        fprintf(stderr, "No thread num");
+        exit(0);
+      }
+      thread_num = ::atoi(args[0]);
+      args.clear();
+    } else {
+      args.emplace_back(argv[argc-1]);
+    }
+    argc--;
   }
 
   EventLoop loop;
   EchoCgiServer server(&loop, InetAddr(port));
-  
+  server.SetLoopNum(thread_num);
   server.Listen();
+
   loop.StartLoop();
 }
