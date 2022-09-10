805bcb602232a5fa9a230214ff653dd0b7d5ee23
Conzxy <1967933844@qq.com>
2022-09-10 22:10:28 +0800

    SendXXX(ChunkList) & UNKNOWN & ABORT

--- fcgi/fcgi_codec.cc
+++ fcgi/fcgi_codec.cc
@@ -9,17 +9,29 @@ using namespace fcgi;
 static constexpr int FCGI_RECORD_HEADER_LENGTH = sizeof(RecordHeader);
 static constexpr int FCGI_BEGIN_REQUEST_BODY_LENGTH = sizeof(BeginRequestBody);
 static constexpr int FCGI_END_REQUEST_BODY_LENGTH = sizeof(EndRequestBody);
+static constexpr int FCGI_UNKNOWN_TYPE_BODY_LENGTH = sizeof(UnknownTypeBody);
+static constexpr int FCGI_UNKNOWN_RECORD_LENGTH = sizeof(UnknownTypeRecord);
+static constexpr uint16_t MAX_CONTENT_LENGTH = (uint16_t)-1;
 
-static_assert(FCGI_RECORD_HEADER_LENGTH == 8, "");
-static_assert(FCGI_BEGIN_REQUEST_BODY_LENGTH == 8, "");
-static_assert(FCGI_END_REQUEST_BODY_LENGTH == 8, "");
+static_assert(FCGI_RECORD_HEADER_LENGTH == 8,
+              "The length of record header must be 8 bytes");
+static_assert(FCGI_BEGIN_REQUEST_BODY_LENGTH == 8,
+              "The length of BEGIN_REQUEST body must be 8 bytes");
+static_assert(FCGI_END_REQUEST_BODY_LENGTH == 8,
+              "The length of END_REQUEST body must be 8 bytes");
+static_assert(FCGI_UNKNOWN_TYPE_BODY_LENGTH == 8,
+              "The length of UNKNOWN_TYPE body must be 8 bytes");
+static_assert(FCGI_UNKNOWN_RECORD_LENGTH ==
+                  FCGI_RECORD_HEADER_LENGTH + FCGI_UNKNOWN_TYPE_BODY_LENGTH,
+              "The length of UnknownType record must equal to the "
+              "(FCGI_RECORD_HEADER_LENGTH + FCGI_UNKNOWN_TYPE_BODY_LENGTH)");
+static_assert(MAX_CONTENT_LENGTH == 65535,
+              "MAX_CONTENT_LENGTH must equal  to maximum of the uint16_t");
 
 #define PARSE_ERR -1
 #define PARSE_SHORT 1
 #define PARSE_OK 0
 #define PARSE_OK_PARTLY 2
-
-#define MAX_CONTENT_LENGTH 65535
 
 FcgiCodec::FcgiCodec(TcpConnectionPtr const &conn)
 {
@@ -28,10 +40,25 @@ FcgiCodec::FcgiCodec(TcpConnectionPtr const &conn)
         for (;;) {
           switch (ParseRequest(conn, buffer)) {
             case PARSE_ERR:
+            {
               LOG_ERROR << "Parse error";
               return;
+            } break;
             case PARSE_SHORT:
+            {
+              LOG_TRACE << "Parse short, waiting entire message";
               return;
+            } break;
+
+            case PARSE_OK_PARTLY:
+            {
+              LOG_TRACE << "Parse ok partly, receive request part";
+            } break;
+
+            case PARSE_OK:
+            {
+              LOG_TRACE << "Parse ok complete, receive entire request";
+            } break;
           }
         }
       });
@@ -40,12 +67,15 @@ FcgiCodec::FcgiCodec(TcpConnectionPtr const &conn)
 int FcgiCodec::ParseRequest(TcpConnectionPtr const &conn, Buffer &buffer)
 {
   uint32_t required_length = 0;
-  while (buffer.GetReadableSize() >= FCGI_RECORD_HEADER_LENGTH) {
+  if (buffer.GetReadableSize() >= FCGI_RECORD_HEADER_LENGTH) {
+    /* Parse record header */
     RecordHeader header;
     memcpy(&header, buffer.GetReadBegin(), FCGI_RECORD_HEADER_LENGTH);
     header.request_id = sock::ToHostByteOrder16(header.request_id);
     header.content_length = sock::ToHostByteOrder16(header.content_length);
 
+    /* We parse entire message only when the buffer has required_length content
+     * at least */
     required_length = header.content_length + header.padding_length +
                       FCGI_RECORD_HEADER_LENGTH;
     if (buffer.GetReadableSize() < required_length) return PARSE_SHORT;
@@ -56,8 +86,11 @@ int FcgiCodec::ParseRequest(TcpConnectionPtr const &conn, Buffer &buffer)
     LOG_TRACE << "Content length: " << header.content_length;
     LOG_TRACE << "Padding length: " << header.padding_length;
 
+    /* Header has consumed */
     buffer.AdvanceRead(FCGI_RECORD_HEADER_LENGTH);
+    /* Convenient for advance later */
     required_length -= FCGI_RECORD_HEADER_LENGTH;
+
     switch (header.type) {
       case FCGI_BEGIN_REQUEST:
       {
@@ -74,7 +107,9 @@ int FcgiCodec::ParseRequest(TcpConnectionPtr const &conn, Buffer &buffer)
 
       case FCGI_ABORT_REQUEST:
       {
-
+        /* FIXME */
+        RemoveRequest(header.request_id);
+        EndRequest(conn, header.request_id, 0, FCGI_REQUEST_COMPLETE);
       } break;
 
       case FCGI_PARAMS:
@@ -88,7 +123,7 @@ int FcgiCodec::ParseRequest(TcpConnectionPtr const &conn, Buffer &buffer)
           LOG_TRACE << "Terminator of PARAMS";
           ParseParams(request);
           buffer.AdvanceRead(header.padding_length);
-          continue;
+          buffer.Shrink(0);
         }
       } break;
 
@@ -101,8 +136,8 @@ int FcgiCodec::ParseRequest(TcpConnectionPtr const &conn, Buffer &buffer)
         } else {
           /* Request complete, can process it */
           request.codec = this;
-          request_handler_(conn, std::move(request));
           buffer.AdvanceRead(required_length);
+          request_handler_(conn, std::move(request));
           return PARSE_OK;
         }
 
@@ -118,12 +153,36 @@ int FcgiCodec::ParseRequest(TcpConnectionPtr const &conn, Buffer &buffer)
       {
 
       } break;
+
+        /* Unknown type request */
+      default:
+      {
+        UnknownTypeRecord unknown_record{
+            .header{
+                .version = FCGI_VERSION_1,
+                .type = FCGI_UNKNOWN_TYPE,
+                .request_id = sock::ToHostByteOrder16(header.request_id),
+                .content_length =
+                    sock::ToHostByteOrder16(FCGI_UNKNOWN_TYPE_BODY_LENGTH),
+                .padding_length = 0,
+            },
+            .body{
+                .type = header.type,
+                .reserved = {0, 0, 0, 0, 0, 0, 0},
+            }};
+
+        conn->Send(&unknown_record, FCGI_UNKNOWN_RECORD_LENGTH);
+        RemoveRequest(header.request_id);
+        Close(conn, nullptr);
+      }
     }
 
     if (buffer.GetReadableSize() >= required_length) {
       LOG_TRACE << "advanced length = " << required_length;
       buffer.AdvanceRead(required_length);
     }
+
+    return PARSE_OK_PARTLY;
   }
 
   return PARSE_SHORT;
@@ -151,8 +210,6 @@ bool FcgiCodec::ParseParams(RequestData &data)
   while (stream.GetReadableSize() > 0) {
     name_len = ReadNamePairLength(stream);
     value_len = ReadNamePairLength(stream);
-    LOG_TRACE << "name_len = " << name_len;
-    LOG_TRACE << "value_len = " << value_len;
     name = stream.RetrieveAsString(name_len);
     value = stream.RetrieveAsString(value_len);
     LOG_TRACE << name << ": " << value;
@@ -188,7 +245,7 @@ static inline void SendStream(TcpConnectionPtr const &conn, FcgiType type,
 
     output.Append(&header, FCGI_RECORD_HEADER_LENGTH);
     /* The header.content_length has converted to network byteorder */
-    output.Append(data+has_written, clen);
+    output.Append(data + has_written, clen);
     has_written += clen;
     LOG_TRACE << "has_written = " << has_written;
     output.Append("\0\0\0\0\0\0\0\0", header.padding_length);
@@ -196,6 +253,7 @@ static inline void SendStream(TcpConnectionPtr const &conn, FcgiType type,
     len -= clen;
   }
 }
+
 void FcgiCodec::SendStdout(TcpConnectionPtr const &conn, uint16_t id,
                            char const *data, size_t len)
 {
@@ -203,11 +261,23 @@ void FcgiCodec::SendStdout(TcpConnectionPtr const &conn, uint16_t id,
   SendStream(conn, FCGI_STDOUT, id, data, len);
 }
 
+void FcgiCodec::SendStdout(TcpConnectionPtr const &conn, uint16_t id,
+    ChunkList &output)
+{
+
+}
+
 void FcgiCodec::SendStderr(TcpConnectionPtr const &conn, uint16_t id,
                            char const *data, size_t len)
 {
   LOG_TRACE << "stderr";
   SendStream(conn, FCGI_STDERR, id, data, len);
+}
+
+void FcgiCodec::SendStderr(TcpConnectionPtr const &conn, uint16_t id,
+    ChunkList &output)
+{
+
 }
 
 static inline void EndTerminator(TcpConnectionPtr const &conn, FcgiType type,
@@ -229,7 +299,8 @@ static inline void EndTerminator(TcpConnectionPtr const &conn, FcgiType type,
   conn->Send(&header, FCGI_RECORD_HEADER_LENGTH);
 }
 
-void FcgiCodec::EndRequest(TcpConnectionPtr const &conn, uint16_t id)
+void FcgiCodec::EndRequest(TcpConnectionPtr const &conn, uint16_t id,
+                           uint32_t as, FcgiProtocolStatus ps)
 {
   LOG_TRACE << "Send END_REQUEST Message";
   LOG_TRACE << "request_id = " << id;
@@ -244,14 +315,13 @@ void FcgiCodec::EndRequest(TcpConnectionPtr const &conn, uint16_t id)
       .reserved = 0,
   };
 
-
   ChunkList output;
   output.Append(&header, FCGI_RECORD_HEADER_LENGTH);
 
-  // EndRequestBody body{0, FCGI_REQUEST_COMPLETE, {0, 0, 0}};
-  // output.Append(&body, FCGI_END_REQUEST_BODY_LENGTH);
-  output.Append32(0);
-  output.Append32(0);
+  EndRequestBody body{.app_status = sock::ToNetworkByteOrder32(as),
+                      .protocol_status = ps,
+                      .reserved = {0, 0, 0}};
+  output.Append(&body, FCGI_END_REQUEST_BODY_LENGTH);
   conn->Send(output);
 }
 
@@ -263,6 +333,27 @@ void FcgiCodec::EndStdout(TcpConnectionPtr const &conn, uint16_t id)
 void FcgiCodec::EndStderr(TcpConnectionPtr const &conn, uint16_t id)
 {
   EndTerminator(conn, FCGI_STDERR, id);
+}
+
+void FcgiCodec::Close(TcpConnectionPtr const &conn, FcgiRequest const *request)
+{
+  if (!request || !IsKeepConnection(request->flags)) {
+    conn->ShutdownWrite();
+  }
+}
+
+void FcgiCodec::RemoveRequest(uint16_t request_id)
+{
+  auto iter = request_map_.find(request_id);
+  /*
+   * If the codec is not nullptr,
+   * the request handler may processing.
+   *
+   * Resource is managed by handler.
+   */
+  if (iter != request_map_.end() && !iter->second.codec) {
+    request_map_.erase(iter);
+  }
 }
 
 FcgiCodec::RequestData::~RequestData() noexcept
--- fcgi/fcgi_codec.h
+++ fcgi/fcgi_codec.h
@@ -5,6 +5,7 @@
 
 #include "fcgi_constant.h"
 #include "fcgi_type.h"
+#include "kanon/buffer/chunk_list.h"
 #include "kanon/net/buffer.h"
 #include "kanon/util/noncopyable.h"
 
@@ -22,8 +23,9 @@ class FcgiCodec : kanon::noncopyable {
     ParamMap param_map;
     kanon::Buffer stdin_stream;
     kanon::Buffer data_stream;
-    FcgiCodec *codec;
-
+    FcgiCodec *codec = nullptr;
+    
+    /** The request resource is managed by RequestHandler */
     ~RequestData() noexcept;
   };
 
@@ -32,37 +34,91 @@ class FcgiCodec : kanon::noncopyable {
       std::function<void(kanon::TcpConnectionPtr const &, RequestData data)>;
 
   using RequestMap = std::unordered_map<uint16_t, RequestData>;
+  using FcgiRequest = FcgiCodec::RequestData;
 
-  FcgiCodec(kanon::TcpConnectionPtr const &conn);
-
-  int ParseRequest(kanon::TcpConnectionPtr const &conn, kanon::Buffer &buffer);
-  bool ParseParams(RequestData &data);
-
-  static void SendStdout(kanon::TcpConnectionPtr const &conn, uint16_t id,
-                         char const *data, size_t len);
-  static void SendStderr(kanon::TcpConnectionPtr const &conn, uint16_t id,
-                         char const *data, size_t len);
+  explicit FcgiCodec(kanon::TcpConnectionPtr const &conn);
   
+  /*----------------------*/
+  /* Output stdout stream */
+  /*----------------------*/
+
+  static void SendStdout(kanon::TcpConnectionPtr const &conn, uint16_t id,
+                         char const *data, size_t len);
+
   static void SendStdout(kanon::TcpConnectionPtr const &conn, uint16_t id,
                          kanon::StringView data)
-  { SendStdout(conn, id, data.data(), data.size()); }
+  {
+    SendStdout(conn, id, data.data(), data.size());
+  }
+
+  static void SendStdout(kanon::TcpConnectionPtr const &conn, uint16_t id,
+                         kanon::ChunkList &output);
+
+  /*----------------------*/
+  /* Output stderr stream */
+  /*----------------------*/
+
+  static void SendStderr(kanon::TcpConnectionPtr const &conn, uint16_t id,
+                         char const *data, size_t len);
 
   static void SendStderr(kanon::TcpConnectionPtr const &conn, uint16_t id,
                          kanon::StringView data)
-  { SendStderr(conn, id, data.data(), data.size()); }
+  {
+    SendStderr(conn, id, data.data(), data.size());
+  }
 
-  static void EndRequest(kanon::TcpConnectionPtr const &conn, uint16_t id);
+  static void SendStderr(kanon::TcpConnectionPtr const &conn, uint16_t id,
+                         kanon::ChunkList &output);
+  
+  /*-------------------*/
+  /* Send terminator   */
+  /*-------------------*/
+
+  static void EndRequest(kanon::TcpConnectionPtr const &conn, uint16_t id, uint32_t app_status=0, FcgiProtocolStatus protocol_status=FCGI_REQUEST_COMPLETE);
+
   static void EndStdout(kanon::TcpConnectionPtr const &conn, uint16_t id);
+
   static void EndStderr(kanon::TcpConnectionPtr const &conn, uint16_t id);
   
+  /*-----------------------*/
+  /* Connection management */
+  /*-----------------------*/
+
+  static void Close(kanon::TcpConnectionPtr const &conn,
+                    FcgiRequest const *request);
+  
+  /*-----------------------*/
+  /* Handler register      */
+  /*-----------------------*/
+
   void SetRequestHandler(RequestHandler handler)
-  { request_handler_ = std::move(handler); }
+  {
+    request_handler_ = std::move(handler);
+  }
+
  private:
+  int ParseRequest(kanon::TcpConnectionPtr const &conn, kanon::Buffer &buffer);
+
+  bool ParseParams(RequestData &data);
+
+  void RemoveRequest(uint16_t request_id);
+  
+  /**
+   * Because FasgCgi allow interleaved request,
+   * and the request may process complete asynchronously.
+   *
+   * We must store the request in a associated container.
+   */
   RequestMap request_map_;
+
+  /**
+   * Handle request
+   * The request owner is transfered to the handler(By move).
+   */
   RequestHandler request_handler_;
 };
 
 using FcgiRequest = FcgiCodec::RequestData;
 
-}  // namespace fcgi
-#endif  // FCGI_CODEC_H_
+} // namespace fcgi
+#endif // FCGI_CODEC_H_
