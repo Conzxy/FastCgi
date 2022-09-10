bff0c2ef6a8033ba26b46c02f080e55820e98fd6
Conzxy <1967933844@qq.com>
2022-09-10 22:48:33 +0800

    Convenient API for id version

--- fcgi/fcgi_codec.h
+++ fcgi/fcgi_codec.h
@@ -24,7 +24,7 @@ class FcgiCodec : kanon::noncopyable {
     kanon::Buffer stdin_stream;
     kanon::Buffer data_stream;
     FcgiCodec *codec = nullptr;
-    
+
     /** The request resource is managed by RequestHandler */
     ~RequestData() noexcept;
   };
@@ -37,7 +37,7 @@ class FcgiCodec : kanon::noncopyable {
   using FcgiRequest = FcgiCodec::RequestData;
 
   explicit FcgiCodec(kanon::TcpConnectionPtr const &conn);
-  
+
   /*----------------------*/
   /* Output stdout stream */
   /*----------------------*/
@@ -45,14 +45,32 @@ class FcgiCodec : kanon::noncopyable {
   static void SendStdout(kanon::TcpConnectionPtr const &conn, uint16_t id,
                          char const *data, size_t len);
 
+  /** Convenient API for id version */
+  static void SendStdout(kanon::TcpConnectionPtr const &conn,
+                         FcgiRequest const &request, char const *data,
+                         size_t len)
+  {
+    SendStdout(conn, request.request_id, data, len);
+  }
+
   static void SendStdout(kanon::TcpConnectionPtr const &conn, uint16_t id,
                          kanon::StringView data)
   {
     SendStdout(conn, id, data.data(), data.size());
   }
 
+  static void SendStdout(kanon::TcpConnectionPtr const &conn,
+                         FcgiRequest const &request, kanon::StringView data)
+  {
+    SendStdout(conn, request.request_id, data);
+  }
+
   static void SendStdout(kanon::TcpConnectionPtr const &conn, uint16_t id,
                          kanon::ChunkList &output);
+
+  // static void SendStdout(kanon::TcpConnectionPtr const &conn, FcgiRequest
+  // const&request,
+  //                        kanon::ChunkList &output);
 
   /*----------------------*/
   /* Output stderr stream */
@@ -61,32 +79,69 @@ class FcgiCodec : kanon::noncopyable {
   static void SendStderr(kanon::TcpConnectionPtr const &conn, uint16_t id,
                          char const *data, size_t len);
 
+  static void SendStderr(kanon::TcpConnectionPtr const &conn,
+                         FcgiRequest const &request, char const *data,
+                         size_t len)
+  {
+    SendStderr(conn, request.request_id, data, len);
+  }
+
   static void SendStderr(kanon::TcpConnectionPtr const &conn, uint16_t id,
                          kanon::StringView data)
   {
     SendStderr(conn, id, data.data(), data.size());
   }
 
+  static void SendStderr(kanon::TcpConnectionPtr const &conn,
+                         FcgiRequest const &request, kanon::StringView data)
+  {
+    SendStderr(conn, request.request_id, data);
+  }
+
   static void SendStderr(kanon::TcpConnectionPtr const &conn, uint16_t id,
                          kanon::ChunkList &output);
-  
+
+  // static void SendStderr(kanon::TcpConnectionPtr const &conn, FcgiRequest
+  // const&request,
+  //                        kanon::ChunkList &output);
   /*-------------------*/
   /* Send terminator   */
   /*-------------------*/
 
-  static void EndRequest(kanon::TcpConnectionPtr const &conn, uint16_t id, uint32_t app_status=0, FcgiProtocolStatus protocol_status=FCGI_REQUEST_COMPLETE);
+  static void
+  EndRequest(kanon::TcpConnectionPtr const &conn, uint16_t id,
+             uint32_t app_status = 0,
+             FcgiProtocolStatus protocol_status = FCGI_REQUEST_COMPLETE);
+
+  static void
+  EndRequest(kanon::TcpConnectionPtr const &conn, FcgiRequest const &request,
+             uint32_t app_status = 0,
+             FcgiProtocolStatus protocol_status = FCGI_REQUEST_COMPLETE)
+  {
+    EndRequest(conn, request.request_id, app_status, protocol_status);
+  }
 
   static void EndStdout(kanon::TcpConnectionPtr const &conn, uint16_t id);
+  static void EndStdout(kanon::TcpConnectionPtr const &conn,
+                        FcgiRequest const &request)
+  {
+    EndStdout(conn, request.request_id);
+  }
 
   static void EndStderr(kanon::TcpConnectionPtr const &conn, uint16_t id);
-  
+  static void EndStderr(kanon::TcpConnectionPtr const &conn,
+                        FcgiRequest const &request)
+  {
+    EndStderr(conn, request.request_id);
+  }
+
   /*-----------------------*/
   /* Connection management */
   /*-----------------------*/
 
   static void Close(kanon::TcpConnectionPtr const &conn,
                     FcgiRequest const *request);
-  
+
   /*-----------------------*/
   /* Handler register      */
   /*-----------------------*/
@@ -102,7 +157,7 @@ class FcgiCodec : kanon::noncopyable {
   bool ParseParams(RequestData &data);
 
   void RemoveRequest(uint16_t request_id);
-  
+
   /**
    * Because FasgCgi allow interleaved request,
    * and the request may process complete asynchronously.
